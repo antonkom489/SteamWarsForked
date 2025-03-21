6ff5f6179c6eca01cc0b0aa491011912737ce547
V8275 <87564199+V8275@users.noreply.github.com>
2025-03-21 22:59:08 +0300

    Revert "upgraded spawner"
    This reverts commit fe3cd4389ab978e5d8acbf0bdd3bc7082c50c226.

--- Source/SteamWars/GameMode/SWGameMode.h
+++ Source/SteamWars/GameMode/SWGameMode.h
@@ -33,7 +33,7 @@ struct FWaveWeaponData : public FTableRowBase
 	TSubclassOf<ARangeWeaponItem> WeaponItemClass;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	ESpawnersID SpawnerWeaponID = ESpawnersID::Weapon_None;
+	ESpawnersID SpawnerID = ESpawnersID::Weapon;
 };
 
 USTRUCT(BlueprintType)
@@ -88,7 +88,6 @@ private:
 	int EnemyRemaining = 0;
 	TMap<TSubclassOf<AEnemyBaseCharacter>, int> EnemyPool;
 	TMap<ESpawnersID, TMap<TSubclassOf<AEnemyBaseCharacter>, int32>> SpawnerEnemyPool;
-	TArray<ESpawnersID> GetWeaponSpawnersForCurrentWave();
 
 	void SpawnEnemy();
 	void StartWave();
