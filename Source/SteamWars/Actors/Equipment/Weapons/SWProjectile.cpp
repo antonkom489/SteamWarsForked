db6ca9a29fadba8b6876534426eab5ffa91e9c60
V8275 <87564199+V8275@users.noreply.github.com>
2025-04-06 22:11:07 +0300

    added projectile

--- Source/SteamWars/Actors/Equipment/Weapons/SWProjectile.cpp
+++ Source/SteamWars/Actors/Equipment/Weapons/SWProjectile.cpp
@@ -1,10 +1,12 @@
 #include "SWProjectile.h"
-#include "GameFramework/ProjectileMovementComponent.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 
 ASWProjectile::ASWProjectile()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("ProjectileMovement"));
@@ -12,9 +14,44 @@ ASWProjectile::ASWProjectile()
 	ProjectileMovement->InitialSpeed = 7000.0f;
 
 	bReplicates = true;
-
-	//TODO change this to a better value
 	NetUpdateFrequency = 100.0f;
+
+	// Получаем ссылку на игрока
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (PlayerController)
+	{
+		// Получаем персонажа игрока
+		ACharacter* PlayerCharacter = Cast<ACharacter>(PlayerController->GetCharacter());
+		if (PlayerCharacter)
+		{
+			// Получаем позицию игрока
+			FVector PlayerLocation = PlayerCharacter->GetActorLocation();
+
+			// Теперь можно использовать PlayerLocation для дальнейших вычислений
+			// Например, передать его в метод Initialize
+			Initialize(PlayerLocation);
+		}
+	}
 }
 
+void ASWProjectile::Initialize(FVector TargetLocation)
+{
+	// Получаем текущее местоположение снаряда
+	FVector ProjectileLocation = GetActorLocation();
+
+	// Вычисляем направление к цели (позиции игрока)
+	FVector Direction = TargetLocation - ProjectileLocation;
+
+	// Нормализуем вектор направления
+	Direction.Normalize();
+
+	// Устанавливаем скорость снаряда в направлении игрока
+	ProjectileMovement->Velocity = Direction * ProjectileMovement->InitialSpeed;
+
+	// Вычисляем вращение из вектора направления
+	FRotator NewRotation = UKismetMathLibrary::MakeRotFromX(Direction);
+
+	// Устанавливаем новое вращение снаряда
+	SetActorRotation(NewRotation);
+}
 
--- Source/SteamWars/Actors/Equipment/Weapons/SWProjectile.h
+++ Source/SteamWars/Actors/Equipment/Weapons/SWProjectile.h
@@ -12,7 +12,12 @@ class STEAMWARS_API ASWProjectile : public AActor
 public:
 	ASWProjectile();
 
+	// Функция для инициализации снаряда
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	void Initialize(FVector TargetLocation);
+
 protected:
+	// Компонент для движения снаряда
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "PBProjectile")
 	class UProjectileMovementComponent* ProjectileMovement;
 };
